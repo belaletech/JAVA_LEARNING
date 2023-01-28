807b3753c343f082aad48af3033577daaa0f9818
belal <belal@DESKTOP-A0RK059>
2023-01-28 23:13:29 +0530

    Addition of Two number by user input

---
+++ JAVA_LEARNING/src/basic/Sum_Of_Two_Number.java
@@ -0,0 +1,18 @@
+package basic;
+import java.util.*;
+public class Sum_Of_Two_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a,b,sum;
+		System.out.print("Enter your first number\n");
+		a=sc.nextInt();
+		System.out.print("Enter your 2nd number\n");
+		b=sc.nextInt();
+		sum=a+b;
+		System.out.print("Sum of Two Number is= "+sum);
+		
+	}
+
+}
