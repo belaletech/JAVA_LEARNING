f77643a44ba77dcbc00a451fc1a4034392dc3ddc
belal <belal@DESKTOP-A0RK059>
2023-01-28 22:49:30 +0530

    1st Commit

---
+++ JAVA_LEARNING/src/basic/Hello_World.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This side belal");
+
+	}
+
+}
---
+++ JAVA_LEARNING/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author belal
+ *
+ */
+module JAVA_LEARNING {
+}
