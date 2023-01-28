b0eedfdd1f6be7904cda28dfe4e0589b658b3de1
belal <belal@DESKTOP-A0RK059>
2023-01-28 23:06:14 +0530

    Area of circle

---
+++ JAVA_LEARNING/src/basic/area_circle.java
@@ -0,0 +1,18 @@
+package basic;
+import java.util.*;
+
+public class area_circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		float area,radius;
+		System.out.println("Enter the value of radius");
+		radius=sc.nextFloat();
+		area=3.14f*radius*radius;
+		System.out.println("Area of circle is ="+area);
+		
+
+	}
+
+}
