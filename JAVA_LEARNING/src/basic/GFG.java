c02e2d18900a1e449c31ba68a8b29b1dbd819daf
belaletech <belalahmadswnbhr@gmail.com>
2023-02-08 15:56:08 +0530

    basic of data type

---
+++ JAVA_LEARNING/src/basic/GFG.java
@@ -0,0 +1,16 @@
+package basic;
+
+class GFG
+{
+    public static void main(String[] args)
+    {
+        Double object = new Double("2.4");
+        int a = object.intValue();
+        byte b = object.byteValue();
+        float d = object.floatValue();
+        double c = object.doubleValue();
+ 
+        System.out.println(a + b + c + d );
+ 
+    }
+}
