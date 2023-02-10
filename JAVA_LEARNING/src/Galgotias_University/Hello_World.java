2b90a42293ea4efeda4eb06dcab82fc2a99373e9
belaletech <belalahmadswnbhr@gmail.com>
2023-02-10 11:45:36 +0530

    Print Hello World

---
+++ JAVA_LEARNING/src/Galgotias_University/Hello_World.java
@@ -0,0 +1,11 @@
+package Galgotias_University;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
