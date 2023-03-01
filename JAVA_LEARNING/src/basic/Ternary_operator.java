8f95862fa2b9e9c56306c5081eb9c5130d0c3fb2
belaletech <belalahmadswnbhr@gmail.com>
2023-03-01 16:47:38 +0530

    update

---
+++ JAVA_LEARNING/src/basic/Ternary_operator.java
@@ -0,0 +1,24 @@
+package basic;
+import java.util.*;
+
+public class Ternary_operator {
+	Scanner sc=new Scanner(System.in);
+	public void sum(int a,int b)
+	{
+		System.out.println("Enter your first number");
+	a=sc.nextInt();
+	System.out.println("Enter your second number");
+	b=sc.nextInt();
+	int z=a+b;
+	System.out.println("Sum of two number is ="+z);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Ternary_operator belal=new Ternary_operator();
+		belal.sum(0,0);
+
+	}
+
+}
