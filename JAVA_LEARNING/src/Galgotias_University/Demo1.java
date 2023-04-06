2335c41b81593cf1806e1d2f4ba0d3dac558be6e
belaletech <belalahmadswnbhr@gmail.com>
2023-04-07 02:09:05 +0530

    update

--- JAVA_LEARNING/src/Galgotias_University/Demo1.java
+++ JAVA_LEARNING/src/Galgotias_University/Demo1.java
@@ -1,5 +1,19 @@
 package Galgotias_University;
+import java.util.*;
 
 public class Demo1 {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number for storing in array");
+		int n=sc.nextInt();
+		
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Element of array"+arr[n]);
+		}
+		
+	}
 
 }
