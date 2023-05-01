b9e04b9de867cc3b3a8c255e9f77ba0ea5487698
belaletech <belalahmadswnbhr@gmail.com>
2023-05-02 00:36:20 +0530

    update

---
+++ JAVA_LEARNING/src/Galgotias_University/GetterSetterExample.java
@@ -0,0 +1,12 @@
+package Galgotias_University;
+
+public class GetterSetterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student();
+		
+
+	}
+
+}
---
+++ JAVA_LEARNING/src/Galgotias_University/Student.java
@@ -0,0 +1,10 @@
+package Galgotias_University;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
