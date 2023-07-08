e3f623e23c67f6665f60f99458882a398ce23ed6
belaletech <belalahmadswnbhr@gmail.com>
2023-07-08 14:16:51 +0530

    update

---
+++ JAVA_LEARNING/src/JavaLab/New.java
@@ -0,0 +1,10 @@
+package JavaLab;
+import OOPS.HelloWorld;
+
+public class New {
+  public static void main(String[] args) {
+    ImportJava.printMessage();
+    HelloWorld ahmad=new HelloWorld();
+    ahmad.belal();
+  }
+}
